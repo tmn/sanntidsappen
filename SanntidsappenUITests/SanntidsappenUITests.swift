7b3da408d482751a022fe3954a2f4e7b76bb8b1c
Tri Nguyen <mail@trimn.net>
2018-11-12 22:40:03 +0100

    Remove example test functions

--- SanntidsappenUITests/SanntidsappenUITests.swift
+++ SanntidsappenUITests/SanntidsappenUITests.swift
@@ -27,9 +27,4 @@ class SanntidsappenUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
 }
