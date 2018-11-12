248d815c683528f8374f9219954459b311d2a3ea
Tri Nguyen <mail@trimn.net>
2018-11-12 22:51:32 +0100

    Add AboutCoordinator with its view controller

---
+++ Sanntidsappen/ViewControllers/AboutViewController.swift
@@ -0,0 +1,28 @@
+//
+// This file is a part of the Sanntidsappen project
+//
+// Copyright © 2018 the Sanntidsappen authors
+// Licensed under Apache License 2.0
+//
+// See LICENSE.txt for license information
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+
+        self.title = NSLocalizedString("About", comment: "About Sanntidsappen")
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
